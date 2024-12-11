1aee9102c5375137887cf7f7a810303959b6c82d
Mitul Patel <me@mitulpa.tel>
2024-12-11 17:32:15 -0500

    fix(cli): use the correct bin instead & try catch

--- src/cli.ts
+++ src/cli.ts
@@ -1,17 +1,29 @@
 #!/usr/bin/env node
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-
 import { displayCard } from "./card";
 
-void yargs(hideBin(process.argv))
-    .command("$0", "Display the business card", {}, async () => {
-        await displayCard();
-    })
-    .option("no-color", {
-        type: "boolean",
-        description: "Disable colors in output",
-        default: false,
-    })
-    .help()
-    .parse();
+async function main() {
+    try {
+        await yargs(hideBin(process.argv))
+            .command("$0", "Display the business card", {}, async () => {
+                await displayCard();
+                process.exit(0);
+            })
+            .option("no-color", {
+                type: "boolean",
+                description: "Disable colors in output",
+                default: false,
+            })
+            .help()
+            .parse();
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
