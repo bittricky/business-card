cc53c11d21088869049aa4066a3f60365120a5af
Mitul Patel <me@mitulpa.tel>
2025-01-01 06:14:16 -0500

    fix(section): update language

--- src/sections/education.ts
+++ src/sections/education.ts
@@ -2,15 +2,15 @@ import { profile } from "../config/profile.js";
 import { formatEducation, formatSection } from "../utils/formatting.js";
 
 export function generateEducation(): string[] {
-    return [
-        formatSection("My Education:"),
-        ...profile.education.map((edu) =>
-            formatEducation(
-                edu.degree,
-                edu.institution,
-                `(${edu.period.start} - ${edu.period.end})`,
-            ),
-        ),
-        "",
-    ];
+  return [
+    formatSection("Education:"),
+    ...profile.education.map((edu) =>
+      formatEducation(
+        edu.degree,
+        edu.institution,
+        `(${edu.period.start} - ${edu.period.end})`
+      )
+    ),
+    "",
+  ];
 }
